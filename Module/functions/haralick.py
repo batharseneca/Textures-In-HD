a9b42d1a75d8f98d302a6c4a44af3a45131e2c5e
Nishanth Merwin <nishanthmerwin@hotmail.com>
2015-12-05 14:16:18 -0500

    Finished haralick features.. not tested

--- Module/functions/haralick.py
+++ Module/functions/haralick.py
@@ -75,10 +75,11 @@ def CORR(CoMat):
 	for i in range(0,CoMat.shape[0]-1)
 		for j in range(0,CoMat.shape[1]-1)
 			val += ((i*j) * CoMat[i][j] - (xmean(CoMat)-ymean(CoMat)))/(xstdev(CoMat)*ystdev(CoMat))
-	return val
+	return val	
 
 # Total Mean
 def mean(CoMat):
+	val = 0
 	for i in range(0,CoMat.shape[0]-1)
 		for j in range(0,CoMat.shape[1]-1)
 			val += CoMat[i][j]
@@ -88,20 +89,75 @@ def mean(CoMat):
 
 # Sum of Squares, Variance
 def variance(CoMat):
+	val = 0
 	for i in range(0,CoMat.shape[0]-1)
 		for j in range(0,CoMat.shape[1]-1)
 			val += (i-mean(CoMat))**2 * CoMat[i][j]
 	return val
 
-# Sum Average
 
 
+# P(x+y) is the diagonal sum of the matrix.
+def xPlusY(CoMat,kValue):
+	val = 0
+	for i in range(0,CoMat.shape[0]-1)
+		for j in range(0,CoMat.shape[1]-1)
+			if(i+j in range(0,kValue):
+				val += CoMat[i][j]
+	return val
 
 
+# Sum Average	
+def sumAverage(CoMat):
+	val = 0
+	for k in range(0,CoMat.shape[0]*2-2)
+		val += xPlusY(CoMat,k) * k
+	return val
+
+# Sum Entropy
+def sumEntropy(coMat):
+	val = 0
+	for k in range(0,CoMat.shape[0]*2-2)
+		val += xPlusY(CoMat,k)* log(xPlusY(CoMat,k))
+	return val*(-1)
+	
+
+	
+# Differencce Entropy
+def difEntropy(coMat):
+	val = 0
+	for k in range(0,CoMat.shape[0]-1)
+		val += xPlusY(CoMat,k)* log(xPlusY(CoMat,k))
+	return val*(-1)
 
 
+# Inertia
+def inertia(coMat):
+	val = 0
+	for i in range(0,CoMat.shape[0]-1)
+		for j in range(0,CoMat.shape[1]-1)
+			val += ((i-j)**2) * CoMat[i][j]
+	return val
+	
+	
+# Cluster Shade
+def clusterShade(coMat):
+	val = 0
+	ux = xmean(coMat)
+	uy = ymean(coMat)
+	for i in range(0,CoMat.shape[0]-1)
+		for j in range(0,CoMat.shape[1]-1)
+			val += (i + j - ux - uy)**3 * CoMat[i][j]
+	
 
-
+# Cluster Prominance
+def clusterShade(coMat):
+	val = 0
+	ux = xmean(coMat)
+	uy = ymean(coMat)
+	for i in range(0,CoMat.shape[0]-1)
+		for j in range(0,CoMat.shape[1]-1)
+			val += (i + j - ux - uy)**4 * CoMat[i][j]
 
 
 
